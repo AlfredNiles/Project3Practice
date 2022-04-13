218315b76ae15655be468e0db95674289cd01ce2
Eric Conrad <eric.conrad@jhu.edu>
2022-04-13 16:07:50 -0400

    Fixed indentation in java.js, added closing brackets at the end of the file.

--- java.js
+++ java.js
@@ -1,16 +1,17 @@
 $(document).ready( function() {
 
-    // This controls the button for the c note.
+  // This controls the button for the c note.
 
-    // This variable calls the cNote audio element in the HTML.
-    var cNote = document.getElementById('cAudio');
+  // This variable calls the cNote audio element in the HTML.
+  var cNote = document.getElementById('cAudio');
 
-    // This turns our <div id= "c"> into a button that plays our audio file when the box is pressed.
-    $('#c').mousedown(function(){
+  // This turns our <div id= "c"> into a button that plays our audio file when the box is pressed.
+  $('#c').mousedown(function(){
 
-        // This is a property that scrubs the audio file back to its start.
-        cNote.currentTime = 0;
+    // This is a property that scrubs the audio file back to its start.
+    cNote.currentTime = 0;
 
-        // This plays the audio file.
-        cNote.play();
-    });
+    // This plays the audio file.
+    cNote.play();
+  });
+});
